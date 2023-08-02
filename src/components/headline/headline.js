f499c878465f374e879d2bd91e79e8f8d1867f1d
emmanuelcortesniku <leitmotivsmusic@gmail.com>
2023-08-02 13:52:33 -0600

    headline component desgn and styling

---
+++ src/components/headline/headline.js
@@ -0,0 +1,13 @@
+import './headline.css'
+
+export default function Headline() {
+    return (
+        <>
+            <div className='headline'>
+                <h2>Make it <br /> awesome.</h2>
+                <p>Al trained to generate realistic 3D objects.</p>
+                <button>Get started</button>
+            </div>
+        </>
+    )
+}
